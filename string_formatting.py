455b8cff0ade43c000bbde84cfda170c38285333
test <test@test.com>
2017-08-01 19:01:16 +0300

    Initial commit

---
+++ string_formatting.py
@@ -0,0 +1,15 @@
+# Using method str to convert variable into string
+age = 27
+print('I\'am ' + str(age+10) + ' old')
+print("I'am {0} years old".format(age))
+
+# Using method format
+print('There are {0} days in {1}, {2}, {3}, {4}'.format(31, 'January', 'March', 'May', 'June'))
+
+age = (252//(40-(5+8)))
+high = ()
+print("Hello, my name is Vadim. I'am " + str(age) + " years old. Ones, I was walking through the park outside"
+                                                    "and saw a small rock.")
+print("There are many rocks in nature. For example, I saw {0} feet rock.".format(2+2))
+
+
